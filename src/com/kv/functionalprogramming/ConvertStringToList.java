e18de9f63f705e430e843854c5a72003e8fd917e
K Vandana <kaurvandana94@gmail.com>
2022-01-24 23:51:27 +0530

    [commit]

--- src/com/kv/functionalprogramming/ConvertStringToList.java
+++ src/com/kv/functionalprogramming/ConvertStringToList.java
@@ -1,16 +1,24 @@
 package com.kv.functionalprogramming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConvertStringToList {
     public static void main(String[] args) {
         String str = "Hello World";
         System.out.println("value of str = " + str);
 
+        //way 1
+        List<Character> charStr = new ArrayList<>();
+        str.chars().forEach(y -> {
+            if(y!= ' ') {
+                charStr.add((char) y);
+            }
+        });
+        System.out.println(charStr);
 
-
-
-
-
-
+        //way 2
         char[] chars = str.toCharArray();
+
     }
 }
