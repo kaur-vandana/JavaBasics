f8d921dcebedccd7fa28d3c3fe36455fa7ee8aa7
kaur-vandana <kvandana.work@gmail.com>
2023-10-14 13:59:06 +0530

    update

--- src/com/kv/structuralprogramming/SumOfOddNumbers.java
+++ src/com/kv/structuralprogramming/SumOfOddNumbers.java
@@ -5,16 +5,36 @@ import java.util.List;
 
 public class SumOfOddNumbers {
     public static void main(String[] args) {
-        List<Integer> numbers =	Arrays.asList(1,3,4,6,2,7);
 
-       int sum =0;
+        List<Integer> numbers = Arrays.asList(1, 3, 4, 6, 2, 7);
 
-       for(int i=0; i<numbers.size(); i++) {
-           if(numbers.get(i)%2 !=0 ) {
-               sum = sum + numbers.get(i);
-           }
-       }
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 2 != 0) {
+                sum = sum + numbers.get(i);
+            }
+        }
+        System.out.println("sum1 = " + sum);
 
-        System.out.println("sum = " + sum);
+        // ................................................................
+
+        sum = 0;
+        for (Integer x : numbers) {
+            if (x % 2 != 0) {
+                sum = sum + x;
+            }
+        }
+        System.out.println("sum2 = " + sum);
+
+        // ................................................................
+
+        sum = numbers.stream()
+                .filter(x -> x % 2 != 0)
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println("sum3 = " + sum);
+
+        // ................................................................
+
     }
 }
