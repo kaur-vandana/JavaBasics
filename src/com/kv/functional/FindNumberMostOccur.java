cb120ff1777eda3548657e9e8f2bdcdcb3a2c0e0
kaur-vandana <kvandana.work@gmail.com>
2023-10-15 14:53:40 +0530

    update

--- src/com/kv/functional/FindNumberMostOccur.java
+++ src/com/kv/functional/FindNumberMostOccur.java
@@ -7,11 +7,16 @@ import java.util.function.BinaryOperator;
 
 public class FindNumberMostOccur {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,3,3,3,3,3,4,5,6,7,8,3,5,5,5,5,5,5,5,5,0,5,5,5);
+        List<Integer> numbers = Arrays.asList(1, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8, 3, 5, 5, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5);
+       
+        
         Integer maxoccur = numbers.stream()
                 .reduce(
-                        BinaryOperator.maxBy((x,y)->Collections.frequency(numbers,x) - Collections.frequency(numbers,y))
-                ).orElse(null);
+                        BinaryOperator
+                                .maxBy((x, y) -> Collections.frequency(numbers, x) - Collections.frequency(numbers, y)))
+                .orElse(null);
+        
+
         System.out.println("max occur element = " + maxoccur);
     }
 
--- src/com/kv/structuraltofunctional/DistinctNumber.java
+++ src/com/kv/structuraltofunctional/DistinctNumber.java
@@ -3,16 +3,23 @@ package com.kv.structuraltofunctional;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DistinctNumber {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,3,3,3,3,3,4,5,6,7,8,3);
+        List<Integer> numbers = Arrays.asList(1, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8, 3);
 
         HashSet<Integer> num = new HashSet<>();
-        for(Integer x : numbers) {
+        for (Integer x : numbers) {
             num.add(x);
         }
 
         System.out.println(num);
+
+        // ................................................................
+
+        List<Integer> distinctNumberList = numbers.stream().distinct().collect(Collectors.toList());
+
+        System.out.println("distinct Number List " + distinctNumberList);
     }
 }
