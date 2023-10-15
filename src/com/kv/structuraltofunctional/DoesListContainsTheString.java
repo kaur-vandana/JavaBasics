8a42bc1552a89e78d76d78a4d46ec2d050a4dea3
kaur-vandana <kvandana.work@gmail.com>
2023-10-15 13:30:07 +0530

    update

--- src/com/kv/structuraltofunctional/DoesListContainsTheString.java
+++ src/com/kv/structuraltofunctional/DoesListContainsTheString.java
@@ -2,6 +2,7 @@ package com.kv.structuraltofunctional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class DoesListContainsTheString {
     public static void main(String[] args) {
@@ -10,5 +11,20 @@ public class DoesListContainsTheString {
         if (words.contains("Baeldung")) {
             System.out.println("Baeldung is in the list");
         }
+
+
+        // ................................................................
+
+
+        Predicate<String> string = obj -> obj.equals("Baeldung");
+
+        /*
+         * anyMatch() method checks whether any Stream element matches
+         * the specified predicate
+         */
+        boolean result = words.stream().anyMatch(string);
+
+        System.out.println(result);
+
     }
 }
